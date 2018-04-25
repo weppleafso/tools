ea1ed91d3efb8ddd13e1a3246f1ced7f207fcbb8
wuzhipeng <weppleafso@163.com>
2018-04-25 17:42:16 +0800

    ++

--- Execl2Json.py
+++ Execl2Json.py
@@ -17,29 +17,61 @@ def readExecl(filename):
         titles = sheet_data.row_values(1) 
         #拿到类型
         types = sheet_data.row_values(2) 
+        findGroup = -1
         find = -1
         table = {}
-        print(titles)
         for i in range(0,len(titles)):
             title = titles[i]
+            print(title)
             if title.startswith('*'):
                 titles[i] = title[1:]
                 find = i
-        if find != -1:
+            if title.startswith('#'):
+                titles[i] = title[1:]
+                findGroup = i
+        if findGroup != -1:
+            if find != -1:
+                main_group_col = sheet_data.col_values(findGroup)
+                main_col = sheet_data.col_values(find)
+                main_group_type = types[findGroup]
+                main_type = types[find]
+                for i in range(4,len(main_group_col)):
+                    content = sheet_data.row_values(i)
+                    if main_group_col[i] != "":
+                        group = getValueByType(main_group_col[i],main_group_type)
+                        if not group in table.keys():
+                            table[group] = {}
+                        id = getValueByType(main_col[i],main_type)
+                        table[group][id] = {}
+                        for k in range(0,len(content)):
+                            if content[k] != '':
+                                value = getValueByType(content[k],types[k])
+                                key = titles[k]
+                                table[group][id][key] = value
+        elif find != -1:
             main_col = sheet_data.col_values(find)
             main_type = types[find]
             for i in range(4,len(main_col)):
                 content = sheet_data.row_values(i)
-                id = getValueByType(main_col[i],main_type)
-                table[id] = {}
-                for k in range(0,len(content)):
-                    if content[k] != '':
-                        value = getValueByType(content[k],types[k])
-                        key = titles[k]
-                        table[id][key] = value
+                if main_col[i] != "":
+                    id = getValueByType(main_col[i],main_type)
+                    table[id] = {}
+                    for k in range(0,len(content)):
+                        if content[k] != '':
+                            value = getValueByType(content[k],types[k])
+                            key = titles[k]
+                            table[id][key] = value
+            
         ret[sheet_name] = table
-    print(ret)
     return ret
+
+def writeFile(filePath,data):
+    data_str = json.dumps(data, sort_keys=True, indent=2,ensure_ascii=False)
+    if os.path.exists(filePath):
+        os.remove(filePath)
+    with open(filePath,'w') as f:
+        f.write(data_str)
+
 
 def getValueByType(value,type1):
     if type1 == "int":
@@ -50,11 +82,32 @@ def getValueByType(value,type1):
         return json.loads(value)
     if type1 == "json":
         return json.loads(value)
-        
-    return value  
+    if type1 == "auto":
+        if is_digit(value):
+            return int(value)
+        if is_num(value):
+            return float(value)
+        if is_json(value):
+            return json.loads(value)
+        return "" + value
+    return "" + value  
+    
+def is_json(text):
+    try:
+        json_object = json.loads(myjson)
+    except ValueError:
+        return False
+    return True
 
+def is_digit(text):
+    ret = "" + text
+    return ret.isdigit()
 
+def is_num(text):
+    ret = "" + text
+    return ret.isalnum()
 
 if __name__ == '__main__':
-    readExecl("test.xlsx")
+    ret = readExecl("test.xlsx")
+    writeFile('mdata.json',ret)
     
